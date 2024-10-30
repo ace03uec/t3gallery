ba18cfeebc5ef13fa30007f379b77044e670027c
Giridharan Manivannan <giridar123@gmail.com>
2024-10-30 22:26:30 +0530

    adds analytics on delete.

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,6 +4,7 @@ import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
 import { images } from "./db/schema";
 import { and, eq } from "drizzle-orm";
+import analyticsServerClient from "./analytics";
 
 
 export async function getMyImages() {
@@ -43,5 +44,13 @@ export async function deleteImage(id: number) {
     await db
         .delete(images)
         .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+    analyticsServerClient.capture({
+        distinctId: user.userId,
+        "event": "delete_image",
+        properties: {
+            imageId: id,
+        }
+    })
 }
 
