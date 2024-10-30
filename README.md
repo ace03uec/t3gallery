1d3e069df4ebd54009d51f41851a03b2316b6879
Giridharan Manivannan <giridar123@gmail.com>
2024-10-30 15:17:39 +0530

    image padding and checkoff router.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 - [X] "taint" server-only
 - [X] Use Next/Image component
 - [X] Error management (w/sentry)
-- [] Routing/image page (parallel route)
+- [X] Routing/image page (parallel route)
 - [] Delete button (w/Server actions)
 - [] Analytics (posthog)
 - [] Ratelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ async function Images() {
   return (
     <>
           <div className="flex flex-wrap justify-center gap-4 p-4">
-      {[...images, ...images, ...images, ...images].map((image) => (
+      {images.map((image) => (
           <div key={image.id} className="w-48 h-48 flex flex-col">
             <Link href={`/img/${image.id}`} >
             <Image src={image.url} style={{objectFit: "contain"}} alt={image.name} className="object-contain" width={480} height={480}/>
