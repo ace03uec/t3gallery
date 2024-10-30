c19db8f8c8c4d64220c00371ae4f9597248533a5
Giridharan Manivannan <giridar123@gmail.com>
2024-10-30 14:38:29 +0530

    image moved to full component and update modal.

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,7 +21,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
       <dialog ref={dialogRef} className="m-0 w-screen h-screen bg-zinc-900/50" onClose={onDismiss}>
         {children}
-        <button onClick={onDismiss} className="close-button" />
+        {/* <button onClick={onDismiss} className="close-button" /> */}
       </dialog>,
     document.getElementById('modal-root')!
   );
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,5 +3,12 @@ import { getImage } from "~/server/queries"
 export default async function FullPageImageView(props: {id: number}) {
     const image = await getImage(props.id)
 
-    return <img className="w-96" src={image.url} alt={image.name} />
+    return <div className="flex w-full h-full bg-green-500 min-w-0">
+            <div className="flex-shrink flex justify-center items-center">
+            <img className="object-contain" src={image.url} alt={image.name} />
+            </div>
+            <div className="w-48 flex flex-col flex-shrink-0 border-l">
+                <div className="text-xl font-bold">{image.name}</div>
+            </div>
+        </div>
 }
